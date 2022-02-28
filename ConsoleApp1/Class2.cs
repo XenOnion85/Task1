53cb455206d6b3b6befffc68aafdac4045660e4d
XenOnion85 <100402573+XenOnion85@users.noreply.github.com>
2022-02-28 09:42:50 +0300

    Add files via upload

---
+++ ConsoleApp1/Class2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class z1
+    {
+        public int Viraj (int a, int b, int z, int x)
+        {
+            int c;
+            c = ((b+z)*(a-b))/x;
+            return c;
+        }
+        public int Login (string A)
+        {
+            int B;
+            if (A == "Админ")
+            {
+                Console.WriteLine("Введите пароль:");
+                B = Convert.ToInt32(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("Я вас не знаю");
+                B = 0;
+            }
+            return B;
+        }
+        public double Adin(double x, double a)
+        {
+            double Y;
+            double А = Math.Pow(x, 5);
+            double Б = (x * Math.Atan(a + x) - Math.Sqrt(Math.Abs(x - a)));
+            double В = Math.Log10(Math.Pow(Math.Abs(x) + 1, 2));
+            Y =  А*Б + В;
+            return Y;
+        }
+        public double Bdin (double a, double x, double b)
+        {
+            double F;
+            F = Math.Sqrt(a * x * x + b * x) * Math.Exp(-a * x) + Math.Log10(Math.Abs(x) + 1);
+            return F;
+        }
+        public int Den (int K, int N)
+        {
+            int D;
+            D = ((K + N - 2) % 7) + 1;
+            return D;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            #region 1
+            //int c;
+            //z1 number = new z1();
+            //Console.WriteLine("Введите 4 переменных a,b,z,x: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int z = Convert.ToInt32(Console.ReadLine());
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //c = number.Viraj(a,b,z,x);
+            //Console.WriteLine("Значение выражения: {0}", c);
+            #endregion
+            #region 2
+            //int B;
+            //z1 par = new z1();
+            //Console.WriteLine("Введите свой логин:");
+            //string A = Console.ReadLine();
+            //B = par.Login(A);
+            #endregion
+            #region 3
+            //double Y;
+            //z1 ad = new z1();
+            //Console.WriteLine("Введите 2 переменные x,a:");
+            //double xa = Convert.ToDouble(Console.ReadLine());
+            //double aa = Convert.ToDouble(Console.ReadLine());
+            //Y = ad.Adin(xa, aa);
+            //Console.WriteLine("Значение выражения: {0}", Y);
+            //double Z;
+            //Z = ad.Bdin(0.5, 9.5, 1.5);
+            //Console.WriteLine("Значение выражения с х=9,5 : {0}", Z);
+            //Z = ad.Bdin(0.5, 1.5, 1.5);
+            //Console.WriteLine("Значение выражения с х=1,5 : {0}", Z);
+            #endregion
+            #region 4
+            int D;
+            z1 den = new z1();
+            Console.WriteLine("Введите день этого года: ");
+            int K = Convert.ToInt32(Console.ReadLine());
+            int N = 6;
+            D = den.Den(K, N);
+            Console.WriteLine("Введенный день недели: {0}", D)
+            #endregion
+        }
+    }
+}
